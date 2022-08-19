e1ea37251182c570e3ed783bcfa0fd17ebff65c6
Niranjan Singh <niranjan.kala@gmail.com>
2022-08-19 15:57:53 +0530

    Added Swagger

--- src/StockAnalyzer.WebApi/App_Start/WebApiConfig.cs
+++ src/StockAnalyzer.WebApi/App_Start/WebApiConfig.cs
@@ -1,19 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Autofac.Integration.WebApi;
+using StockAnalyzer.WebApi.Handler;
+//using StockAnalyzer.WebApi.Logger;
 using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
 
 namespace StockAnalyzer.WebApi
 {
+    /// <summary>
+    /// The main class <c>WebApiConfig</c>.
+    /// Represents the default api configuration
+    /// </summary>
     public static class WebApiConfig
     {
+        /// <summary>
+        /// Registers the api configuration 
+        /// </summary>
+        /// <param name="config">The Http configuration object to configure.</param>
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
 
+            //config.DependencyResolver = new AutofacWebApiDependencyResolver(AutofacConfig.Container);
+            //var logger = config.DependencyResolver.GetService(typeof(GlobalExceptionLogger));
+            // config.Services.Replace(typeof(IExceptionLogger), logger);
+            //config.Services.Replace(typeof(IExceptionHandler), new GenericExceptionHandler());
             // Web API routes
             config.MapHttpAttributeRoutes();
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ src/StockAnalyzer.WebApi/Handler/GlobalExceptionHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+
+namespace StockAnalyzer.WebApi.Handler
+{
+    /// <summary>
+    /// The main class <c>GenericExceptionHandler</c>.
+    /// Handles the execptions of the api.
+    /// </summary>
+    public class GenericExceptionHandler : ExceptionHandler
+    {
+        /// <summary>
+        /// Custom Web API global exception handler
+        /// </summary>
+        /// <param name="context">ExceptionHandlerContext</param>
+        /// <param name="cancellationToken"></param>
+        public async override Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+        {
+            HttpStatusCode exceptionStatus;
+            string errorMessage = "";
+            if (context.Exception is NotImplementedException)
+            {
+                exceptionStatus = HttpStatusCode.NotImplemented;
+                errorMessage = "Unavailable content";
+            }
+            else if (context.Exception is InvalidOperationException)
+            {
+                exceptionStatus = HttpStatusCode.BadRequest;
+                errorMessage = "invalid operation";
+            }
+            else if (context.Exception is FileNotFoundException)
+            {
+                exceptionStatus = HttpStatusCode.NotFound;
+                errorMessage = "File not found";
+            }
+            else if (context.Exception is FormatException)
+            {
+                exceptionStatus = HttpStatusCode.InternalServerError;
+                errorMessage = context.Exception.Message;
+            }
+            else if (context.Exception is ArgumentException)
+            {
+                exceptionStatus = HttpStatusCode.BadRequest;
+                errorMessage = context.Exception.Message;
+            }
+            else
+            {
+                errorMessage = "An unexpected error occurred";
+                exceptionStatus = HttpStatusCode.InternalServerError;
+            }
+
+            context.Result = await Task<IHttpActionResult>.Run(() =>
+             {
+                 return new GenericErrorResult(context.Request, exceptionStatus, errorMessage);
+             });
+        }
+    }
+}
